95ce46a4b53784027f76503498ba86eb96a00d93
Deepak Jadhav <137857979+DeepxkJadhav@users.noreply.github.com>
2025-09-06 17:06:34 +0530

    recommender.py

--- recommender.py
+++ recommender.py
@@ -1,15 +1,21 @@
 import pandas as pd
+import joblib
+import os
+
+MODEL_PATH = "models/food_model.pkl"
 
 def load_data(path="data/foods.csv"):
     return pd.read_csv(path)
 
-def recommend_foods(goal="focus"):
-    df = load_data()
-    if goal == "focus":
-        return df[df['focus_boost'] == 1]
-    elif goal == "memory":
-        return df[df['memory_boost'] == 1]
-    elif goal == "anti-stress":
-        return df[df['anti_stress'] == 1]
-    else:
-        return df
+def load_model():
+    model = joblib.load(MODEL_PATH)
+    goal_enc = joblib.load("models/goal_encoder.pkl")
+    food_enc = joblib.load("models/food_encoder.pkl")
+    return model, goal_enc, food_enc
+
+def predict_food(age, goal):
+    model, goal_enc, food_enc = load_model()
+    goal_encoded = goal_enc.transform([goal])[0]
+    prediction = model.predict([[age, goal_encoded]])
+    food = food_enc.inverse_transform(prediction)[0]
+    return food
